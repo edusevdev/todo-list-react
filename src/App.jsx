bb8a81e0646bcaf509e27fac169c69b2e92691f9
edusevdev <57229032+edusevdev@users.noreply.github.com>
2023-10-08 18:05:18 +0200

    Main page: Components, crud of task, validations and notifications (#1)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import { TodoList } from './components/TodoList'
+
+class App extends React.Component {
+  render() {
+    return (
+      <TodoList title={'TODO List React'}/>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,54 @@
+import React from "react"
+import { useState } from "react"
+import { TaskInput } from "./TaskInput"
+import TaskList from "./TaskList"
+import toast, { Toaster } from 'react-hot-toast'
+
+export const TodoList = ({title}) => {
+
+  const [taskList, setTaskList] = useState([{name: 'Buy water'}, {name: 'Clean car'}])
+
+  const addTask = (name) => {
+
+    if (!taskList.some(item => item.name === name)) {
+      const task = {name: name};
+      const updatedList = [...taskList, task]
+      setTaskList(updatedList)
+    } else {
+      showToastExistingTask()
+    }
+  }
+
+  const removeTask = (name) => {
+    const updatedList = taskList.filter(item => item.name !== name)
+    setTaskList(updatedList)
+  }
+
+  const showToastExistingTask = () => toast.success('This task already exists', {
+    style: {
+      border: '1px solid #713200',
+      padding: '16px',
+      color: '#713200',
+    },
+    iconTheme: {
+      primary: '#713200',
+      secondary: '#FFFAEE',
+    },
+  });
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <div id="todo-list-container">
+        <TaskInput addTask={addTask} />
+        <TaskList taskList={taskList} handleRemoveTask={removeTask} />
+      </div>
+      <div id="toast-notification">
+      <Toaster
+        position="bottom-right"
+        reverseOrder={false}
+      />
+      </div>
+    </>
+  )
+}
